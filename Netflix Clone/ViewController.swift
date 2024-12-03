4981d7aaa49bc3c3d8c50e791c1678c9fa82eb57
Manya Malhotra <malhotra.manyaaaa@gmail.com>
2024-12-03 09:59:32 +0000

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Manya Malhotra on 03/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
